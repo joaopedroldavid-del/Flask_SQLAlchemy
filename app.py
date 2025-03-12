c1680791422ea4adc62763b2d49ee7a2167cc06d
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-03-11 21:44:30 -0300

    Database and conection

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from models.user import User
+from database import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretKey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)
+
+@app.route("/helloworld", methods=["GET"])
+def helloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
