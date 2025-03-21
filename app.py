2f5dbe89f1a6c032b5e56b77f782bc2cdb264af2
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-03-21 19:43:39 -0300

    Route: Delete user

--- app.py
+++ app.py
@@ -81,7 +81,12 @@ def update_user(id_user):
 def delete_user(id_user):
     user = User.query.get(id_user)
 
+    if id_user == current_user.id:
+        return jsonify({"message": "Deleção não permitida"}), 403
+
     if user:
+        db.session.delete(user)
+        db.session.commit()
         return jsonify({"message": f"Usuário {user.username} deletado com sucesso"})
 
     return jsonify({"message": "Usuário não encontrado"}), 404
