45605e4b227e92766c0670e3005cd018c1dac1de
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-03-24 21:24:38 -0300

    Docker and Swap of database

--- models/user.py
+++ models/user.py
@@ -3,5 +3,5 @@ from flask_login import UserMixin
 
 class User (db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True, nullable=False)
-    password = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
