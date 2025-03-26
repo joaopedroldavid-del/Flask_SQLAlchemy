333cd2120e4640d7e62de649a01fedb4cc36c150
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-03-25 23:27:15 -0300

    Cryptography and Levels user

--- models/user.py
+++ models/user.py
@@ -3,5 +3,6 @@ from flask_login import UserMixin
 
 class User (db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')
